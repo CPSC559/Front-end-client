42fe2871e7da95b983b42cd6c7bfda58a1cc727b
Max Botham <maxsspamemails@gmail.com>
2024-02-18 14:00:44 -0700

    Socket setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 
 function App() {
+  useEffect(() => {
+    const socket = io('http://localhost:4000');
+
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.disconnect();
+      console.log('Disconnected from server');
+    };
+  }, []); // empty array means run on mount
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App with WebSocket</h1>
     </div>
   );
 }
